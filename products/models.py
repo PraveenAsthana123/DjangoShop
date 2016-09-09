4fac2153b44dd92bc9240af60e30997448db4303
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-09-08 20:31:21 -0500

    Create view for the products app - Implement urls - Create templates - Install python packages: pep8, flake8, jedi

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
