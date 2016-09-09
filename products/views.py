b927e1defe98744a08a7846d2df74a5c986caa75
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-09-09 13:57:28 -0500

    Add product detail template

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Product
+from django.shortcuts import get_object_or_404
 
 
 def hello_world(request):
@@ -8,5 +9,14 @@ def hello_world(request):
     template = loader.get_template('index.html')
     context = {
         'products': products
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    template = loader.get_template('product_detail.html')
+    context = {
+        'product': product
     }
     return HttpResponse(template.render(context, request))
