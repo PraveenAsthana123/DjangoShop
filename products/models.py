0f89ed36b848210475fbe01169a368e135edece7
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-09-09 14:43:31 -0500

    Add form to add new products

--- products/models.py
+++ products/models.py
@@ -11,3 +11,6 @@ class Product(models.Model):
 
     def __str__(self):
         return '%s' % self.name
+
+    class Meta:
+        ordering = ('id',)
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Product
 from django.shortcuts import get_object_or_404
+from .models import Product
+from .forms import ProductForm
 
 
 def hello_world(request):
@@ -18,5 +19,20 @@ def product_detail(request, pk):
     template = loader.get_template('product_detail.html')
     context = {
         'product': product
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def new_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ProductForm()
+    template = loader.get_template('new_product.html')
+    context = {
+        'form': form
     }
     return HttpResponse(template.render(context, request))
